85fa07f8aaf6763e0f250543826e32b3406410c6
Ryan Norman <rsnorman15@gmail.com>
2016-01-23 00:26:14 -0500

    Scaffold out team junk

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122035848) do
+ActiveRecord::Schema.define(version: 20160123052122) do
 
   create_table "repos", force: :cascade do |t|
     t.string   "company"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160122035848) do
   end
 
   add_index "repos", ["owner_id"], name: "index_repos_on_owner_id"
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
