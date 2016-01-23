76a4fcaa20db447fa5a68cb86ca087e44a55904f
Ryan Norman <rsnorman15@gmail.com>
2016-01-23 13:46:59 -0500

    Ignore has and belongs to many offense

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,5 +4,5 @@ class Team < ActiveRecord::Base
   validates :leader, presence: true
 
   belongs_to :leader, class_name: User
-  has_and_belongs_to_many :repos
+  has_and_belongs_to_many :repos # rubocop:disable Rails/HasAndBelongsToMany
 end
