b54d595cd6f1feba48bcd910549002dd8b1dd6ec
Ryan Norman <rsnorman15@gmail.com>
2016-01-24 16:53:29 -0500

    Remove uneeded comments

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -3,22 +3,16 @@ class ReposController < ApplicationController
   respond_to :html, :json
   load_and_authorize_resource
 
-  # POST /repos
-  # POST /repos.json
   def create
     @repo.save
     respond_with @repo
   end
 
-  # PATCH/PUT /repos/1
-  # PATCH/PUT /repos/1.json
   def update
     @repo.update(repo_params)
     respond_with @repo
   end
 
-  # DELETE /repos/1
-  # DELETE /repos/1.json
   def destroy
     @repo.destroy
     respond_with @repo
@@ -26,8 +20,6 @@ class ReposController < ApplicationController
 
   private
 
-  # Never trust parameters from the scary internet,
-  # only allow the white list through.
   def repo_params
     params.require(:repo).permit(
       :company, :organization, :name, :description, :url
