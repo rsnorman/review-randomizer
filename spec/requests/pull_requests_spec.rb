389643c387ede90de0c6a1a46a4f4395faf4b248
Ryan Norman <rsnorman15@gmail.com>
2016-01-25 23:33:16 -0500

    Removing pending tests

--- spec/requests/pull_requests_spec.rb
+++ spec/requests/pull_requests_spec.rb
@@ -2,10 +2,5 @@ require 'rails_helper'
 
 RSpec.describe 'PullRequests', type: :request do
   describe 'GET /pull_requests' do
-    it 'works! (now write some real specs)' do
-      pending
-      get pull_requests_path
-      expect(response).to have_http_status(200)
-    end
   end
 end
--- spec/requests/teams_spec.rb
+++ spec/requests/teams_spec.rb
@@ -2,10 +2,5 @@ require 'rails_helper'
 
 RSpec.describe 'Teams', type: :request do
   describe 'GET /teams' do
-    it 'works! (now write some real specs)' do
-      pending
-      get teams_path
-      expect(response).to have_http_status(200)
-    end
   end
 end
