234f988012f35e381933bb90c919f1dc75d33b77
Ryan Norman <rsnorman15@gmail.com>
2016-01-27 08:23:11 -0500

    Add guard clause for password hint

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,7 @@
 # View helper for user junk
 module UsersHelper
   def password_hint
-    if @minimum_password_length
-      "<em>(#{@minimum_password_length} characters minimum)</em>".html_safe
-    end
+    return unless @minimum_password_length
+    "<em>(#{@minimum_password_length} characters minimum)</em>".html_safe
   end
 end
