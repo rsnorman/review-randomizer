2cd9e7cc65f0a421ef2db537a91578e21743939c
Ryan Norman <rsnorman15@gmail.com>
2016-01-31 11:16:45 -0500

    Test create json view for pull request

--- app/views/api/v1/pull_requests/create.json.jbuilder
+++ app/views/api/v1/pull_requests/create.json.jbuilder
@@ -2,8 +2,8 @@ json.pull_request do
   json.id @pull_request.id
   json.title @pull_request.title
   json.number @pull_request.number
-  json.created_at @pull_request.created_at
-  json.updated_at @pull_request.updated_at
+  json.created_at @pull_request.created_at.iso8601
+  json.updated_at @pull_request.updated_at.iso8601
   json.review_assignments @pull_request.review_assignments.map(&:handle)
 
   json.actions do
---
+++ spec/views/api/v1/pull_requests/create.json.jbuilder_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'api/v1/pull_requests/create', type: :view do
+  let(:repo) { FactoryGirl.create(:repo) }
+  let(:pull_request) { FactoryGirl.create(:pull_request, repo: repo) }
+  let(:review_assignment1) { FactoryGirl.create(:review_assignment) }
+  let(:review_assignment2) { FactoryGirl.create(:review_assignment) }
+
+  before do
+    pull_request.review_assignments << review_assignment1
+    pull_request.review_assignments << review_assignment2
+  end
+
+  before(:each) do
+    assign(:pull_request, pull_request)
+  end
+
+  it 'renders pull request JSON' do
+    render
+    rendered_json = JSON.parse(rendered)['pull_request']
+    expect(rendered_json['id']).to eq pull_request.id
+    expect(rendered_json['title']).to eq pull_request.title
+    expect(rendered_json['number']).to eq pull_request.number
+    expect(rendered_json['created_at']).to eq pull_request.created_at.iso8601
+    expect(rendered_json['updated_at']).to eq pull_request.updated_at.iso8601
+  end
+
+  it 'renders review assignemtn JSON' do
+    render
+    rendered_json = JSON.parse(rendered)['pull_request']
+    expect(rendered_json['review_assignments'].first)
+      .to eq review_assignment1.handle
+    expect(rendered_json['review_assignments'].second)
+      .to eq review_assignment2.handle
+  end
+
+  it 'renders pull request actions' do
+    render
+    rendered_json = JSON.parse(rendered)['pull_request']
+    expect(rendered_json['actions']['show']).to eq(
+      "http://test.host:80/repos/#{repo.id}/pull_requests/#{pull_request.id}"
+    )
+  end
+end
