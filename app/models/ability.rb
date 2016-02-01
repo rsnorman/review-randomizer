513cc1e58d8e6260bba9ed617c2ddad00acd87ea
Ryan Norman <rsnorman15@gmail.com>
2016-01-31 19:42:33 -0500

    Test more realistic ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -42,25 +42,19 @@ class Ability
   def create_pull_request_abilities
     can :create,  PullRequest
     can :read,    PullRequest, repo: { company_id: user.company_id }
-    can :update,  PullRequest, author_id: user.id
-    can :destroy, PullRequest, author_id: user.id
-    can :update,  PullRequest, repo: { owner_id: user.id }
-    can :destroy, PullRequest, repo: { owner_id: user.id }
+    can :manage,  PullRequest, author_id: user.id
+    can :manage,  PullRequest, repo: { owner_id: user.id }
   end
 
   def create_team_membership_abilities
-    can :create,  TeamMembership, team: { leader_id: user.id }
+    can :manage,  TeamMembership, team: { leader_id: user.id }
     can :read,    TeamMembership, team: { company_id: user.company_id }
-    can :update,  TeamMembership, team: { leader_id: user.id }
-    can :destroy, TeamMembership, team: { leader_id: user.id }
   end
 
   def create_review_assignment_abilities
-    can :create,  ReviewAssignment
-    can :read,    ReviewAssignment, team: { company_id: user.company_id }
-    can :update,  ReviewAssignment, team: { leader_id: user.id }
-    can :destroy, ReviewAssignment, team_membership: { team: { leader_id: user.id } }
-    can :update,  ReviewAssignment, pull_request: { author_id: user.id }
-    can :destroy, ReviewAssignment, pull_request: { author_id: user.id }
+    can :manage, ReviewAssignment, pull_request: { repo: { owner_id: user.id } }
+    can :manage, ReviewAssignment, pull_request: { author_id: user.id }
+    can :manage, ReviewAssignment, team: { leader_id: user.id }
+    can :read,   ReviewAssignment, team: { company_id: user.company_id }
   end
 end
