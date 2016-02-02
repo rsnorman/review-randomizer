ed8903cc3aeb3b2ea34f3ac81cdd63efebe627b5
Ryan Norman <rsnorman15@gmail.com>
2016-02-02 01:34:43 -0500

    Capture name and company in errors

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -14,12 +14,12 @@ Rollbar.configure do |config|
   # `username`, and `email` methods to fetch those properties. To customize:
   # config.person_method = "my_current_user"
   # config.person_id_method = "my_id"
-  # config.person_username_method = "my_username"
+  config.person_username_method = "name"
   # config.person_email_method = "my_email"
 
   # If you want to attach custom data to all exception and message reports,
   # provide a lambda like the following. It should return a hash.
-  # config.custom_data_method = lambda { {:some_key => "some_value" } }
+  config.custom_data_method = lambda { { company: current_company.name } }
 
   # Add exception class names to the exception_level_filters hash to
   # change the level that exception is reported at. Note that if an exception
